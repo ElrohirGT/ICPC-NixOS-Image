be9336fa229097e25ab65fdd84dd9ba02c3292f6
ElrohirGT <elrohirgt@gmail.com>
2026-02-15 21:34:48 -0600

    feat: Added other niceties

--- hosts/gnome/configuration.nix
+++ hosts/gnome/configuration.nix
@@ -10,11 +10,48 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  users.users.alice = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    initialPassword = "test";
+  networking.hostName = "gnome";
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Guatemala";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_GT.UTF-8";
+    LC_IDENTIFICATION = "es_GT.UTF-8";
+    LC_MEASUREMENT = "es_GT.UTF-8";
+    LC_MONETARY = "es_GT.UTF-8";
+    LC_NAME = "es_GT.UTF-8";
+    LC_NUMERIC = "es_GT.UTF-8";
+    LC_PAPER = "es_GT.UTF-8";
+    LC_TELEPHONE = "es_GT.UTF-8";
+    LC_TIME = "es_GT.UTF-8";
   };
+
+  users.users.contestant = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "networkmanager"];
+    initialPassword = "icpc";
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  nix.settings = {
+    show-trace = true;
+    trusted-users = ["root"];
+  };
+
+  hardware.graphics.enable = true;
+  security.sudo.enable = false;
+  xdg.portal.enable = true;
+
+  programs.java.enable = true;
 
   system.stateVersion = "25.11";
 }
